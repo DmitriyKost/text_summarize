0c8776d89921c1b2754ad48ad57e45919c6df647
DmitriyKost <dima3588858@gmail.com>
2024-01-20 14:31:41 +0300

    Added receive text handler

--- pkg/handlers.go
+++ pkg/handlers.go
@@ -2,9 +2,30 @@ package pkg
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hello from backend")
+}
+
+func ReceiveText(w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodPost {
+        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+        return
+    }
+    body, err := io.ReadAll(r.Body)
+    if err != nil {
+        http.Error(w, "Error reading request body", http.StatusInternalServerError)
+    }
+    defer r.Body.Close()
+    text := string(body)
+
+    // handling text TODO!!!
+    fmt.Printf("%s", text)
+
+    w.WriteHeader(http.StatusOK)
+    // return summarized text TODO!!!
+    w.Write([]byte("Text received"))
 }
